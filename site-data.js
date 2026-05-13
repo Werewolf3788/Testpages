2cb53b77f1cfabf87de5b3b735ee97bda6131d37
Werewolf <rwyt8dnnjy@privaterelay.appleid.com>
2026-05-13 14:43:02 -0400

    Create site-data.js

---
+++ site-data.js
@@ -0,0 +1,70 @@
+/**
+ * S&K VENTURE SIGNS - MASTER DATA CONFIGURATION
+ * This file replaces the Google Sheets. Edit variables here to update all pages automatically.
+ */
+
+const SK_DATA = {
+    // 1. GLOBAL SITE SETTINGS (Used on every page for Footer/Nav/Modals)
+    global: {
+        phones: ["618-708-4450", "844-907-4450"],
+        email: "Scott.Robinson@skventuresigns.com",
+        addressStreet: "607 W Clark Ave",
+        addressCity: "Effingham IL 62401",
+        navigation: [
+            { label: "Home", url: "./index.html" },
+            { label: "Services", url: "./services.html" },
+            { label: "Locations", url: "./locations.html" },
+            { label: "Fair", url: "./fair.html" },
+            { label: "Clients", url: "./clients.html" },
+            { label: "Contact", url: "./contact.html" }
+        ]
+    },
+
+    // 2. THE "IMAGE WITH URL" FORMULA FOR CLIENTS
+    // Copy a block {} and paste it below to add a new client.
+    clients: [
+        {
+            name: "Example Client 1",
+            url: "https://theirwebsite.com",
+            image: "https://skventuresigns.com/wp-content/uploads/2025/03/client1.png" // Can be a Google Drive link
+        },
+        {
+            name: "Example Client 2",
+            url: "https://anotherwebsite.com",
+            image: "https://skventuresigns.com/wp-content/uploads/2025/03/client2.png"
+        }
+    ],
+
+    // 3. SERVICES (Home Page & Services Page)
+    services: [
+        {
+            title: "Digital Sign Rental",
+            description: "Flexible rental options for high-quality digital displays.",
+            btnLabel: "Learn More",
+            btnUrl: "./services.html",
+            image: "https://raw.githubusercontent.com/Werewolf3788/SKVentureSigns/b3ae21594b6a28420e4eb4937ff2f237b7889df0/images/signs.PNG"
+        },
+        {
+            title: "Sign Maintenance & Repair",
+            description: "Comprehensive maintenance and repair services.",
+            btnLabel: "Get Support",
+            btnUrl: "./contact.html",
+            image: "https://raw.githubusercontent.com/Werewolf3788/SKVentureSigns/b3ae21594b6a28420e4eb4937ff2f237b7889df0/images/signs1.PNG"
+        }
+    ],
+
+    // 4. LOCATIONS INVENTORY
+    locations: [
+        {
+            town: "Effingham",
+            county: "Effingham",
+            address: "Route 40",
+            info: "High traffic digital board.",
+            lat: 39.1243,
+            long: -88.5524,
+            price: "$300/mo",
+            pdf: "https://skventuresigns.com/specs.pdf",
+            specs: { size: "8x16", traffic: "15,000 VPD" }
+        }
+    ]
+};
