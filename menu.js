ae1f04b59ea79a40b0a80d5f45f9cd932fcc758f
Werewolf <rwyt8dnnjy@privaterelay.appleid.com>
2026-06-16 03:08:26 -0400

    Add menu items with names and URLs

---
+++ menu.js
@@ -0,0 +1,26 @@
+[
+  {
+    "name": "Home",
+    "url": "https://staging.supportmylocalcommunity.com/"
+  },
+  {
+    "name": "Clay City",
+    "url": "https://staging.supportmylocalcommunity.com/clay-city/"
+  },
+  {
+    "name": "Sailor Springs",
+    "url": "https://staging.supportmylocalcommunity.com/sailor-springs/"
+  },
+  {
+    "name": "Xenia",
+    "url": "https://staging.supportmylocalcommunity.com/xenia/"
+  },
+  {
+    "name": "Flora",
+    "url": "https://ourflora.com"
+  },
+  {
+    "name": "Louisville",
+    "url": "https://staging.supportmylocalcommunity.com/louisville/"
+  }
+]
